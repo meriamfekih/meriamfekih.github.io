c20ce1edbcdf8169dd885266d8231a3de47c49db
fekih <mfekih@mobelite.fr>
2024-01-09 15:17:01 +0100

    build: add static files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { FormsModule} from '@angular/forms';
-import { SendMailService } from './send-mail.service';
-import { HttpClientModule } from '@angular/common/http';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-
-
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { SlickCarouselModule } from "ngx-slick-carousel";
+import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from "./about/about.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @NgModule({
   declarations: [
@@ -23,13 +19,12 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     PortfolioComponent,
   ],
   imports: [
-      FormsModule,
-      HttpClientModule,
-      BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
     AppRoutingModule,
-      SlickCarouselModule
+    SlickCarouselModule,
   ],
-  providers: [SendMailService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
